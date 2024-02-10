a57ad0c1743a7b10312f1e23c8198909167b55e7
Jayden Hendrickse <jayden.herberhendrickse@gmail.com>
2024-02-10 19:17:33 +0200

    challenge 3: polygons

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ raph.shape("turtle")
 raph.color("red")
 
 
-## Challenge 2
+# Challenge 2
 for i in range(10):
     raph.down()
     raph.forward(10)
     raph.up()
     raph.forward(10)
     
-
+# Challenge 3
+def draw_polygon(num_sides):
+    angle = 360 / num_sides
+    for i in range(num_sides):
+        raph.forward(100)
+        raph.right(angle)
 
 
 
