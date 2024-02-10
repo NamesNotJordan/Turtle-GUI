3fae8d0d28cdb1a83d3c0b41064e0b110455a424
Jayden Hendrickse <jayden.herberhendrickse@gmail.com>
2024-02-10 19:12:36 +0200

    Did first couple challenges

---
+++ main.py
@@ -0,0 +1,19 @@
+from turtle import Turtle, Screen
+
+raph = Turtle()
+raph.shape("turtle")
+raph.color("red")
+
+
+## Challenge 2
+for i in range(10):
+    raph.down()
+    raph.forward(10)
+    raph.up()
+    raph.forward(10)
+    
+
+
+
+
+screen = Screen()
